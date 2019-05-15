86a2960357d98a796382b020825cc595f3062526
pengzhaopeng <17688700269@163.com>
2019-05-15 18:18:16 +0800

    #SpringBoot基础和高级部分总结

--- src/test/java/com/pengzhaopeng/springbootjsp/SpringBootJspApplicationTests.java
+++ src/test/java/com/pengzhaopeng/springbootjsp/SpringBootJspApplicationTests.java
@@ -5,9 +5,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.sql.DataSource;
+import java.io.File;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -28,6 +33,28 @@ public class SpringBootJspApplicationTests {
 
     @Test
     public void contextLoads() throws SQLException {
+    }
+
+    @Autowired
+    JavaMailSenderImpl javaMailSender;
+
+    @Test
+    public void test2() throws MessagingException {
+        //1、创建一个复杂的消息邮件
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+        //邮件设置
+        helper.setSubject("通知-今晚开会");
+        helper.setText("<b style='color:red'> 今天7:30开会</b>",true);
+
+        helper.setTo("17688700269@163.com");
+        helper.setFrom("763745153@qq.com");
+
+        //上传文件
+        helper.addAttachment("1.jpg",new File("C:\\Users\\Administrator\\Desktop\\表情包\\v2-38e420cbc4b71732e529cfcd7642ee1c_hd.jpg"));
+        //helper.addAttachment("2.jpg",new File(""));
+        javaMailSender.send(mimeMessage);
     }
 
 }
